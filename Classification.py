ff549af1a29ba780db7bc100bd0510a9a33e1ffd
Shafi001s <shafiahmed001s@gmail.com>
2025-11-17 14:16:57 +0000

    added final gradient descent

--- Classification.py
+++ Classification.py
@@ -4,8 +4,9 @@ import copy, math
 
 
 
+
 x_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
-y_train = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1) 
+y_train = np.array([0, 0, 0, 1, 1, 1])
 
 w_tmp = np.array([2,3])
 b_tmp = -1
@@ -114,10 +115,56 @@ def compute_gradient_logistic(X, y, w, b):
 
 dj_dw_tmp, dj_db_tmp = compute_gradient_logistic(x_train, y_train, w_tmp, b_tmp)
 print(f"dj_db: {dj_db_tmp}" )
-print(f"dj_dw: {dj_dw_tmp.tolist()}" )
+print(f"dj_dw: {dj_dw_tmp.tolist()}" ) #tolist for listing the numbers
+
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters): 
+    """
+    Performs batch gradient descent
+    
+    Args:
+      X (ndarray (m,n)   : Data, m examples with n features
+      y (ndarray (m,))   : target values
+      w_in (ndarray (n,)): Initial values of model parameters  
+      b_in (scalar)      : Initial values of model parameter
+      alpha (float)      : Learning rate
+      num_iters (scalar) : number of iterations to run gradient descent
+      
+    Returns:
+      w (ndarray (n,))   : Updated values of parameters
+      b (scalar)         : Updated value of parameter 
+    """
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_dw, dj_db = compute_gradient_logistic(X, y, w, b)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+      
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( compute_cost_logistic(X, y, w, b) )
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
+        
+    return w, b, J_history         #return final w,b and J history for graphing
+alph = 0.1
+iters = 10000
+
+w_out, b_out, _ = gradient_descent(x_train, y_train, w_tmp, b_tmp, alph, iters) 
+print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
 
 
 
+
+"""
 #Generate an array of evenly spaced value from -10 to 10
 z_tmp = np.arange(-10,11)
 
@@ -195,7 +242,6 @@ ax.set_ylabel(r'$x_1$')
 ax.set_xlabel(r'$x_0$')
 plt.show()
 
-
-
+"""
 
     
